92096ebcebbdba7e9aa4d50b7580d231ae7986bc
cperceful <cpercef1@gmail.com>
2016-12-18 00:42:07 -0600

    Added a comment as a test

--- helpers.py
+++ helpers.py
@@ -1,5 +1,5 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
+#here is a comment
 def alphabet_position(char):
 
     char = char.lower();
